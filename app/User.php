2fa456b1f3cf023160a700ae9de672d6ff9e6378
Jose Carreres <jose.carreres.81@gmail.com>
2020-05-28 20:03:28 +0200

    Configurando el modelo user

--- app/User.php
+++ app/User.php
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'surname',
+        'role',
+        'email',
+        'password',
+        'description',
+        'image'
     ];
 
     /**
